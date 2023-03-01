13335365b98d95d578b07a7c9ed63cb4634e7734
connor <connor@DESKTOP-RP4CGVO>
2023-03-01 21:30:14 +0000

    Robofriends functionality

---
+++ src/Containers/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import CardList from "../Components/CardList";
+import SearchBox from "../Components/SearchBox";
+import "./index.css";
+import Scroll from "../Components/Scroll";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // creating the state
+      robots: [],
+      searchBox: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => this.setState({ robots: data }));
+  }
+
+  // function for updating state on search bar input
+  onSearchChange = (event) => {
+    this.setState({ searchBox: event.target.value });
+  };
+  // render function for rendering the final page
+  render() {
+    // search bar logic for state change to robot array
+    // filters robots that include search input
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchBox.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./Containers/index.css";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import App from "./App";
+import App from "./Containers/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
